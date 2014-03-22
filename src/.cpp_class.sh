443c64b4125a2ab3f86c9d3a48e7b5a6508770b1
Casper B. Hansen <casperbhansen@me.com>
2014-03-22 13:06:56 +0100

    Added .cpp_class.sh

---
+++ src/.cpp_class.sh
@@ -0,0 +1,65 @@
+#
+#!/bin/sh
+#
+# cpp_class.sh
+#
+
+if [ "$2" != "" ]; then
+	project=$2
+else
+	echo "Missing project name."
+	exit 2
+fi
+
+if [ "$1" != "" ]; then
+	
+	class=$1 # should capitalize
+	upper=${class^^}
+	echo "/*
+ * $project
+ *
+ * $class.h
+ *
+ */
+
+#ifndef ${upper}_H
+#define ${upper}_H
+
+class $class
+{
+public:
+	$class();
+	~$class();
+
+protected:
+private:
+};
+
+#endif // ${upper}_H
+" > $class.h
+
+	echo "/*
+ * $project
+ *
+ * $class.cpp
+ *
+ */
+
+#include \"$class.h\"
+
+$class::$class()
+{
+	// constructor
+}
+
+$class::~$class()
+{
+	// destructor
+}
+" > $class.cpp
+
+else
+	echo "Missing class name."
+	exit 1
+fi
+
